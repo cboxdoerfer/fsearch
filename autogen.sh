12d0d320eacb18c7f5b729a3e6f4e1d5445a4ff9
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2016-08-06 19:12:26 +0200

    fix travis build

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-aclocal --install -I build/autotools || exit 1
 glib-gettextize --force --copy || exit 1
 intltoolize --force --copy --automake || exit 1
 autoreconf --force --install -Wno-portability || exit 1
