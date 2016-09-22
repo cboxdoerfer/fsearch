1957b208664fbfd813c72c263cd74b3ce4fbd8ef
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2016-09-22 21:50:48 +0200

    use custom css for status bar toggle button

--- src/fsearch_window.c
+++ src/fsearch_window.c
@@ -749,12 +749,12 @@ fsearch_application_window_init (FsearchApplicationWindow *self)
 
     gtk_widget_init_template (GTK_WIDGET (self));
 
-    GtkStyleContext *context = gtk_widget_get_style_context (self->scrolledwindow1);
     GtkCssProvider *provider = gtk_css_provider_new ();
     gtk_css_provider_load_from_resource (provider, "/org/fsearch/fsearch/shared.css");
-    gtk_style_context_add_provider (context,
+    gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
                                     GTK_STYLE_PROVIDER (provider),
                                     GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_object_unref (provider);
 
     create_view_and_model (self);
     //self->selection_popover = create_popover (self->selection_toggle_button);
