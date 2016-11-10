88d1704f994b197e344e920b7ea2856ac1782759
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2016-11-10 19:01:58 +0100

    fix memleak in clipboard handling and refactor some of its code

--- src/clipboard.c
+++ src/clipboard.c
@@ -23,6 +23,18 @@
 static GdkDragAction clipboard_action = GDK_ACTION_DEFAULT;
 static GList* clipboard_file_list = NULL;
 
+enum {
+    URI_LIST = 1,
+    GNOME_COPIED_FILES,
+    N_CLIPBOARD_TARGETS
+};
+
+static GtkTargetEntry targets[] =
+{
+    {"text/uri-list", 0, URI_LIST},
+    {"x-special/gnome-copied-files", 0, GNOME_COPIED_FILES}
+};
+
 static void
 clipboard_clean_data (GtkClipboard *clipboard,
                       gpointer user_data)
@@ -42,28 +54,28 @@ clipboard_get_data (GtkClipboard *clipboard,
                     guint info,
                     gpointer user_data)
 {
-    if (!clipboard_file_list)
+    if (!clipboard_file_list) {
         return ;
+    }
 
     GString *list = g_string_sized_new (8192);
     gboolean use_uri = FALSE;
-    GdkAtom uri_list_target = gdk_atom_intern("text/uri-list", FALSE);
-    GdkAtom gnome_target = gdk_atom_intern("x-special/gnome-copied-files", FALSE);
 
-    if (gtk_selection_data_get_target (selection_data) == gnome_target)
-    {
+    if (info == GNOME_COPIED_FILES) {
         const gchar *action = clipboard_action == GDK_ACTION_MOVE ? "cut\n" : "copy\n";
         g_string_append(list, action);
         use_uri = TRUE;
     }
-    else if (gtk_selection_data_get_target (selection_data) == uri_list_target) {
+    else if (info == URI_LIST) {
         use_uri = TRUE;
+    }
+    else {
+        goto out;
     }
 
 
     GList* l = NULL;
-    for (l = clipboard_file_list; l; l = l->next)
-    {
+    for (l = clipboard_file_list; l; l = l->next) {
         gchar* file_name = NULL;
         if (use_uri) {
             file_name = g_filename_to_uri((char*) l->data, NULL, NULL);
@@ -75,7 +87,7 @@ clipboard_get_data (GtkClipboard *clipboard,
         g_free(file_name);
 
         if (l->next != NULL) {
-            if (gtk_selection_data_get_target (selection_data) != uri_list_target) {
+            if (info == GNOME_COPIED_FILES) {
                 g_string_append_c(list, '\n');
             }
             else {
@@ -88,37 +100,19 @@ clipboard_get_data (GtkClipboard *clipboard,
                             gtk_selection_data_get_target (selection_data),
                             8,
                             (guchar*) list->str, list->len + 1);
-    //g_printf ("clipboard data:\n\"%s\"\n\n", list->str);
+
+out:
     g_string_free(list, TRUE);
 }
 
 void
 clipboard_copy_file_list (GList* file_list, guint32 copy)
 {
-    GtkTargetList* target_list = gtk_target_list_new (NULL, 0);
-
-    gtk_target_list_add_text_targets (target_list, 0);
-    gint n_targets;
-    GtkTargetEntry *targets = gtk_target_table_new_from_list (target_list, &n_targets);
-    n_targets += 2;
-    targets = g_renew (GtkTargetEntry, targets, n_targets);
-
-    GtkTargetEntry *new_target = g_new0 (GtkTargetEntry, 1);
-    new_target->target = "x-special/gnome-copied-files";
-    g_memmove(&(targets[ n_targets - 2 ]), new_target, sizeof (GtkTargetEntry));
-
-    new_target = g_new0(GtkTargetEntry, 1);
-    new_target->target = "text/uri-list";
-    g_memmove (&(targets[ n_targets - 1 ]), new_target, sizeof (GtkTargetEntry));
-
-    gtk_target_list_unref (target_list);
-
     GtkClipboard * clip = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
-    gtk_clipboard_set_with_data (clip, targets, n_targets,
+    gtk_clipboard_set_with_data (clip, targets, G_N_ELEMENTS (targets),
                                  clipboard_get_data,
                                  clipboard_clean_data,
                                  NULL);
-    g_free(targets);
 
     clipboard_file_list = file_list;
     clipboard_action = copy ? GDK_ACTION_COPY : GDK_ACTION_MOVE;
