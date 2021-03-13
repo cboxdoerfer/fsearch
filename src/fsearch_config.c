9f680b2d10fa5b2c607eb0c56a100f9e9bb270fa
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2021-03-13 12:13:45 +0100

    fix compatibility with glib < 2.60

--- src/fsearch_config.c
+++ src/fsearch_config.c
@@ -586,6 +586,25 @@ config_list_compare(GList *l1, GList *l2, bool (*cmp_func)(void *, void *)) {
     return true;
 }
 
+#if !GLIB_CHECK_VERSION(2, 60, 0)
+// Copied from glib for backwards compatibility
+static gboolean
+g_strv_equal(const gchar *const *strv1, const gchar *const *strv2) {
+    g_return_val_if_fail(strv1 != NULL, FALSE);
+    g_return_val_if_fail(strv2 != NULL, FALSE);
+
+    if (strv1 == strv2)
+        return TRUE;
+
+    for (; *strv1 != NULL && *strv2 != NULL; strv1++, strv2++) {
+        if (!g_str_equal(*strv1, *strv2))
+            return FALSE;
+    }
+
+    return (*strv1 == NULL && *strv2 == NULL);
+}
+#endif
+
 FsearchConfigCompareResult
 config_cmp(FsearchConfig *c1, FsearchConfig *c2) {
     FsearchConfigCompareResult result = {};
