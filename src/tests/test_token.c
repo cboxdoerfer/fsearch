35ddefa84df6d6f525102ee669d96f24b526bdf6
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2021-06-18 20:46:50 +0200

    tests: add initial test support

---
+++ src/tests/test_token.c
@@ -0,0 +1,38 @@
+#include <glib.h>
+#include <stdlib.h>
+
+#include "../fsearch_token.h"
+
+typedef struct TestQuery {
+    const char *query;
+    int num_expected_token;
+} TestQuery;
+
+int
+main(int argc, char *argv[]) {
+    TestQuery test_queries[] = {
+        {"only_token", 1},
+        {"first_token second_token", 2},
+        {"\"only token\"", 1},
+        {"\"first token\" second_token", 2},
+        {"\"first and only\"token", 1},
+        {"first\\ and\\ only\\ token", 1},
+        {"first\\ token second\\ token", 2},
+    };
+
+    const int num_test_queries = sizeof(test_queries) / sizeof(test_queries[0]);
+    for (int i = 0; i < num_test_queries; i++) {
+        FsearchToken **tokens = fsearch_tokens_new(test_queries[i].query, false, false, true);
+        g_assert(tokens != NULL);
+
+        int num_token = 0;
+        for (uint32_t j = 0; tokens[j] != NULL; j++) {
+            g_print("%d: token %d: %s\n", i, j, tokens[j]->text);
+            num_token++;
+        }
+        g_assert(num_token == test_queries[i].num_expected_token);
+
+        g_clear_pointer(&tokens, fsearch_tokens_free);
+        g_assert(tokens == NULL);
+    }
+}
