92b8897a607d57a8ffe40df8e6c6fb43a7ab5de4
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2021-06-23 13:26:32 +0200

    token_test: make sure token strings match with their expected results

--- src/tests/test_token.c
+++ src/tests/test_token.c
@@ -6,18 +6,41 @@
 typedef struct TestQuery {
     const char *query;
     int num_expected_token;
+    char **expected_tokens;
 } TestQuery;
+
+char **
+new_strv(int num, ...) {
+    va_list arguments;
+
+    va_start(arguments, num);
+
+    char **ret = calloc(num + 1, sizeof(char *));
+
+    for (int i = 0; i < num; i++) {
+        ret[i] = g_strdup(va_arg(arguments, const char *));
+    }
+    ret[num] = NULL;
+
+    va_end(arguments); // Cleans up the list
+
+    return ret;
+}
+
+#define Q_TEST(q, n, ...) {q, n, new_strv(n, ##__VA_ARGS__)}
 
 int
 main(int argc, char *argv[]) {
     TestQuery test_queries[] = {
-        {"only_token", 1},
-        {"first_token second_token", 2},
-        {"\"only token\"", 1},
-        {"\"first token\" second_token", 2},
-        {"\"first and only\"token", 1},
-        {"first\\ and\\ only\\ token", 1},
-        {"first\\ token second\\ token", 2},
+        Q_TEST("only_token", 1, "only_token"),
+        Q_TEST("first_token second_token", 2, "first_token", "second_token"),
+        Q_TEST("\"only token\"", 1, "only token"),
+        Q_TEST("\"first token\" second_token", 2, "first token", "second_token"),
+        Q_TEST("\"first and only\"token", 1, "first and onlytoken"),
+        Q_TEST("first\\ and\\ only\\ token", 1, "first and only token"),
+        Q_TEST("first\\ token second\\ token", 2, "first token", "second token"),
+        Q_TEST("first_token              second_token", 2, "first_token", "second_token"),
+        Q_TEST("\"first     token\" second_token", 2, "first     token", "second_token"),
     };
 
     const int num_test_queries = sizeof(test_queries) / sizeof(test_queries[0]);
@@ -28,8 +51,12 @@ main(int argc, char *argv[]) {
         int num_token = 0;
         for (uint32_t j = 0; tokens[j] != NULL; j++) {
             g_print("%d: token %d: %s\n", i, j, tokens[j]->text);
+            g_assert(g_strcmp0(tokens[j]->text, test_queries[i].expected_tokens[j]) == 0);
             num_token++;
         }
+        g_clear_pointer(&test_queries[i].expected_tokens, g_strfreev);
+        g_assert(test_queries[i].expected_tokens == NULL);
+
         g_assert(num_token == test_queries[i].num_expected_token);
 
         g_clear_pointer(&tokens, fsearch_tokens_free);
