2f49fb0a0c58cc0da04ff70d62417ab3dee52704
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2022-03-14 15:34:01 +0100

    tests: add test for fsearch_time_parse_range

---
+++ src/tests/test_time_utils.c
@@ -0,0 +1,45 @@
+#include <glib.h>
+
+#include <src/fsearch_time_utils.h>
+
+static void
+test_parse_time_range(void) {
+    typedef struct {
+        const char *string;
+        gboolean expected_success;
+        time_t expected_time_start;
+        time_t expected_time_end;
+        int64_t expected_end_idx;
+    } FsearchTestTimeRangeParseContext;
+
+    FsearchTestTimeRangeParseContext strings[] = {
+        {"2022", TRUE, 1640991600, 1672527600, strlen("2022")},
+        {"abc", FALSE, 0, 0, 0},
+        {"today", TRUE, 0, 0, strlen("today")},
+        {"today", TRUE, 0, 0, strlen("today")},
+        {"yesterday", TRUE, 0, 0, strlen("yesterday")},
+        {"2022-01", TRUE, 0, 0, strlen("2022-01")},
+        {"22-01", TRUE, 0, 0, strlen("22-01")},
+        {"22-1", TRUE, 0, 0, strlen("22-1")},
+        {"22-1-1", TRUE, 0, 0, strlen("22-1-1")},
+    };
+
+    for (gint i = 0; i < G_N_ELEMENTS(strings); ++i) {
+        FsearchTestTimeRangeParseContext *ctx = &strings[i];
+        time_t time_start = 0;
+        time_t time_end = 0;
+        char *end_ptr = NULL;
+        gboolean res = fsearch_time_parse_range(ctx->string, &time_start, &time_end, &end_ptr);
+        g_assert_true(res == ctx->expected_success);
+        // g_assert_cmpint(time_start, ==, ctx->expected_time_start);
+        // g_assert_cmpint(time_end, ==, ctx->expected_time_end);
+        g_assert_cmpstr(end_ptr, ==, ctx->string + ctx->expected_end_idx);
+    }
+}
+
+int
+main(int argc, char *argv[]) {
+    g_test_init(&argc, &argv, NULL);
+    g_test_add_func("/FSearch/time_utils/parse_time_range", test_parse_time_range);
+    return g_test_run();
+}
