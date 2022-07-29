cb979b8e6a0959fb2a1c0eeb44fc244012617789
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2022-07-29 09:48:51 +0200

    test: time_utils: add more test cases

--- src/tests/test_time_utils.c
+++ src/tests/test_time_utils.c
@@ -9,31 +9,54 @@ test_parse_time_interval(void) {
         gboolean expected_success;
         time_t expected_time_start;
         time_t expected_time_end;
-        int64_t expected_end_idx;
     } FsearchTestTimeIntervalParseContext;
 
     FsearchTestTimeIntervalParseContext strings[] = {
-        {"2022", TRUE, 1640991600, 1672527600, strlen("2022")},
-        {"abc", FALSE, 0, 0, 0},
-        {"today", TRUE, 0, 0, strlen("today")},
-        {"today", TRUE, 0, 0, strlen("today")},
-        {"yesterday", TRUE, 0, 0, strlen("yesterday")},
-        {"2022-01", TRUE, 0, 0, strlen("2022-01")},
-        {"22-01", TRUE, 0, 0, strlen("22-01")},
-        {"22-1", TRUE, 0, 0, strlen("22-1")},
-        {"22-1-1", TRUE, 0, 0, strlen("22-1-1")},
+        {"2000abc", FALSE, -1, -1},
+        {"abc2000", FALSE, -1, -1},
+        {"abc", FALSE, -1, -1},
+
+        {"today", TRUE, -1, -1},
+        {"yesterday", TRUE, -1, -1},
+        {"thishour", TRUE, -1, -1},
+        {"pastyear", TRUE, -1, -1},
+        {"past4year", FALSE, -1, -1},
+        {"pastyears", FALSE, -1, -1},
+        {"past3years", TRUE, -1, -1},
+        {"lastweek", TRUE, -1, -1},
+        {"last2weeks", TRUE, -1, -1},
+        {"lasttwoweeks", TRUE, -1, -1},
+        {"lastweeks", FALSE, -1, -1},
+        {"inthelastday", TRUE, -1, -1},
+        {"4months", TRUE, -1, -1},
+        {"4month", FALSE, -1, -1},
+        {"3minuets", TRUE, -1, -1},
+
+        {"2022", TRUE, 1640991600, 1672527600},
+        {"22", TRUE, 1640991600, 1672527600},
+        {"2022-01", TRUE, -1, -1},
+        {"22-01", TRUE, -1, -1},
+        {"22-1", TRUE, -1, -1},
+        {"22-1-1", TRUE, -1, -1},
+        {"22-1-1 12:00:00", TRUE, -1, -1},
+        {"2022-01-01 12:00:00", TRUE, -1, -1},
+        {"2022-01-01 12:00", TRUE, -1, -1},
+        {"2022-01-01 12", TRUE, -1, -1},
+        {"2022-01 12:00:00", FALSE, -1, -1},
+        {"1960", FALSE, -1, -1},
     };
 
     for (gint i = 0; i < G_N_ELEMENTS(strings); ++i) {
         FsearchTestTimeIntervalParseContext *ctx = &strings[i];
         time_t time_start = 0;
         time_t time_end = 0;
-        char *end_ptr = NULL;
+
         gboolean res = fsearch_date_time_parse_interval(ctx->string, &time_start, &time_end);
         g_assert_true(res == ctx->expected_success);
-        // g_assert_cmpint(time_start, ==, ctx->expected_time_start);
-        // g_assert_cmpint(time_end, ==, ctx->expected_time_end);
-        // g_assert_cmpstr(end_ptr, ==, ctx->string + ctx->expected_end_idx);
+        if (res == TRUE && ctx->expected_time_start != -1) {
+            g_assert_cmpint(time_start, ==, ctx->expected_time_start);
+            g_assert_cmpint(time_end, ==, ctx->expected_time_end);
+        }
     }
 }
 
