614c59e2adbf8992009c795188cd234af8bdbfcb
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2026-06-22 19:07:56 +0200

    file_utils: fix broken strverscmp by importing recent glibc implementation

--- src/fsearch_file_utils.c
+++ src/fsearch_file_utils.c
@@ -32,6 +32,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 
 const char *data_folder_name = "fsearch";
@@ -712,91 +713,49 @@ fsearch_file_utils_get_info(const char *path, time_t *mtime, off_t *size, bool *
 // Based on strverscmp from GNU glibc, with slight modification to make sure
 // full paths are sorted properly.
 //
-/* Compare strings while treating digits characters numerically.
-   Copyright (C) 1997, 2002, 2005 Free Software Foundation, Inc.
-   This file is part of the libiberty library.
-   Contributed by Jean-Fran�ois Bignolles <bignolle@ecoledoc.ibp.fr>, 1997.
 
-   Libiberty is free software; you can redistribute it and/or
+// Make sure path seperators sort before any other characters
+static inline int
+path_char_weight(unsigned char c) {
+    if (c == '\0') {
+        return 0; // Null terminator is always the absolute lowest
+    }
+    if (c == G_DIR_SEPARATOR) {
+        return 1; // Path separator is strictly greater than \0, but less than everything else
+    }
+    // Shift all other characters up by 1 to prevent collisions with G_DIR_SEPARATOR
+    return c + 1;
+}
+
+/* Compare strings while treating digits characters numerically.
+   Copyright (C) 1997-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Jean-François Bignolles <bignolle@ecoledoc.ibp.fr>, 1997.
+
+   The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
 
-   Libiberty is distributed in the hope that it will be useful,
+   The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301 USA.  */
-
-/*
-@deftypefun int strverscmp (const char *@var{s1}, const char *@var{s2})
-The @code{strverscmp} function compares the string @var{s1} against
-@var{s2}, considering them as holding indices/version numbers.  Return
-value follows the same conventions as found in the @code{strverscmp}
-function.  In fact, if @var{s1} and @var{s2} contain no digits,
-@code{strverscmp} behaves like @code{strcmp}.
-
-Basically, we compare strings normally (character by character), until
-we find a digit in each string - then we enter a special comparison
-mode, where each sequence of digits is taken as a whole.  If we reach the
-end of these two parts without noticing a difference, we return to the
-standard comparison mode.  There are two types of numeric parts:
-"integral" and "fractional" (those  begin with a '0'). The types
-of the numeric parts affect the way we sort them:
-
-@itemize @bullet
-@item
-integral/integral: we compare values as you would expect.
-
-@item
-fractional/integral: the fractional part is less than the integral one.
-Again, no surprise.
-
-@item
-fractional/fractional: the things become a bit more complex.
-If the common prefix contains only leading zeroes, the longest part is less
-than the other one; else the comparison behaves normally.
-@end itemize
-
-@smallexample
-strverscmp ("no digit", "no digit")
-    @result{} 0    // @r{same behavior as strcmp.}
-strverscmp ("item#99", "item#100")
-    @result{} <0   // @r{same prefix, but 99 < 100.}
-strverscmp ("alpha1", "alpha001")
-    @result{} >0   // @r{fractional part inferior to integral one.}
-strverscmp ("part1_f012", "part1_f01")
-    @result{} >0   // @r{two fractional parts.}
-strverscmp ("foo.009", "foo.0")
-    @result{} <0   // @r{idem, but with leading zeroes only.}
-@end smallexample
-
-This function is especially useful when dealing with filename sorting,
-because filenames frequently hold indices/version numbers.
-@end deftypefun
-
-*/
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
 
 /* states: S_N: normal, S_I: comparing integral part, S_F: comparing
-           fractional parts, S_Z: idem but with leading Zeroes only */
+           fractionnal parts, S_Z: idem but with leading Zeroes only */
 #define S_N 0x0
-#define S_I 0x4
-#define S_F 0x8
-#define S_Z 0xC
+#define S_I 0x3
+#define S_F 0x6
+#define S_Z 0x9
 
 /* result_type: CMP: return diff; LEN: compare using len_diff/diff */
 #define CMP 2
 #define LEN 3
-
-// Make sure path seperators sort before any other characters
-static inline int
-path_char_weight(unsigned char c) {
-    return (c == G_DIR_SEPARATOR) ? 1 : c;
-}
 
 /* Compare S1 and S2 as strings holding indices/version numbers,
    returning less than, equal to or greater than zero if S1 is less than,
@@ -806,57 +765,50 @@ int
 fsearch_file_utils_cmp_paths(const char *s1, const char *s2) {
     const unsigned char *p1 = (const unsigned char *)s1;
     const unsigned char *p2 = (const unsigned char *)s2;
-    unsigned char c1, c2;
-    int state;
-    int diff;
 
-    /* Symbol(s)    0       [1-9]   others  (padding)
-       Transition   (10) 0  (01) d  (00) x  (11) -   */
-    static const unsigned int next_state[] = {/* state    x    d    0    - */
-                                              /* S_N */ S_N,
-                                              S_I,
-                                              S_Z,
-                                              S_N,
-                                              /* S_I */ S_N,
-                                              S_I,
-                                              S_I,
-                                              S_I,
-                                              /* S_F */ S_N,
-                                              S_F,
-                                              S_F,
-                                              S_F,
-                                              /* S_Z */ S_N,
-                                              S_F,
-                                              S_Z,
-                                              S_Z};
+    /* Symbol(s)    0       [1-9]   others
+       Transition   (10) 0  (01) d  (00) x   */
+    static const uint8_t next_state[] = {/* state    x    d    0  */
+                                         /* S_N */ S_N,
+                                         S_I,
+                                         S_Z,
+                                         /* S_I */ S_N,
+                                         S_I,
+                                         S_I,
+                                         /* S_F */ S_N,
+                                         S_F,
+                                         S_F,
+                                         /* S_Z */ S_N,
+                                         S_F,
+                                         S_Z};
 
-    static const int result_type[] = {/* state   x/x  x/d  x/0  x/-  d/x  d/d  d/0  d/-
-                                                 0/x  0/d  0/0  0/-  -/x  -/d  -/0  -/- */
+    static const int8_t result_type[] = {/* state   x/x  x/d  x/0  d/x  d/d  d/0  0/x  0/d  0/0  */
 
-                                      /* S_N */ CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP, CMP, CMP, CMP, CMP,
-                                      CMP,           CMP, CMP, CMP,
-                                      /* S_I */ CMP, -1,  -1,  CMP, +1,  LEN, LEN, CMP, +1,  LEN, LEN, CMP,
-                                      CMP,           CMP, CMP, CMP,
-                                      /* S_F */ CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP, CMP, CMP, CMP, CMP,
-                                      CMP,           CMP, CMP, CMP,
-                                      /* S_Z */ CMP, +1,  +1,  CMP, -1,  CMP, CMP, CMP, -1,  CMP, CMP, CMP};
+                                         /* S_N */ CMP, CMP, CMP, CMP, LEN, CMP, CMP, CMP, CMP,
+                                         /* S_I */ CMP, -1,  -1,  +1,  LEN, LEN, +1,  LEN, LEN,
+                                         /* S_F */ CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
+                                         /* S_Z */ CMP, +1,  +1,  -1,  CMP, CMP, -1,  CMP, CMP};
 
     if (p1 == p2)
         return 0;
 
-    c1 = *p1++;
-    c2 = *p2++;
+    unsigned char c1 = *p1++;
+    unsigned char c2 = *p2++;
     /* Hint: '0' is a digit too.  */
-    state = S_N | ((c1 == '0') + (isdigit(c1) != 0));
+    int state = S_N + ((c1 == '0') + (isdigit(c1) != 0));
 
-    while ((diff = path_char_weight(c1) - path_char_weight(c2)) == 0 && c1 != '\0') {
+    int diff;
+    while ((diff = path_char_weight(c1) - path_char_weight(c2)) == 0) {
+        if (c1 == '\0')
+            return diff;
+
         state = next_state[state];
         c1 = *p1++;
         c2 = *p2++;
-        state |= (c1 == '0') + (isdigit(c1) != 0);
+        state += (c1 == '0') + (isdigit(c1) != 0);
     }
 
-    state = result_type[state << 2 | (((c2 == '0') + (isdigit(c2) != 0)))];
+    state = result_type[state * 3 + (((c2 == '0') + (isdigit(c2) != 0)))];
 
     switch (state) {
     case CMP:
